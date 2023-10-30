bc45beeec625bfe6acf8eba1e70322a445d0be4e
Irman Zaf <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2023-10-30 13:39:53 +0800

    feat: done with progressModel.js

--- src/SQL/models/progressModel.js
+++ src/SQL/models/progressModel.js
@@ -17,13 +17,45 @@ function selectTaskProgressById(data, callback) {
         WHERE progress_id = ?;
     `;
 
-	const VALUES = [data.progress];
+	const VALUES = [data.progress_id];
 
 	pool.query(SQLQUERY, VALUES, callback);
 }
 
 function updateTaskProgressById(data, callback) {
 	const SQLQUERY = `
-
+        UPDATE TaskProgress
+        SET user_id = ?, task_id = ?, completion_date = ?, notes = ?
+        WHERE progress_id = ?;
     `;
+
+	const VALUES = [
+		data.user_id,
+		data.task_id,
+		data.completion_date,
+		data.notes,
+		data.progress_id,
+	];
+
+	pool.query(SQLQUERY, VALUES, callback);
 }
+
+function deleteTaskProgressById(data, callback) {
+	const SQLQUERY = `
+        DELETE FROM TaskProgress
+        WHERE progress_id = ?;
+
+        ALTER TABLE User AUTO_INCREMENT = 1;
+    `;
+
+	const VALUES = [data.progress_id];
+
+	pool.query(SQLQUERY, VALUES, callback);
+}
+
+module.exports = {
+	insertNewTaskProgress,
+	selectTaskProgressById,
+	updateTaskProgressById,
+	deleteTaskProgressById,
+};
