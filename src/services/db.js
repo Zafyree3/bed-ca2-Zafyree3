ccd632b78a624f45e987943929f788c3603e15a7
Irman Zaf. <irman_zafyree_bin_irmanizam@s2019.ssts.edu.sg>
2023-10-30 23:09:24 +0800

    !fix: made db.js use data from .env

--- src/services/db.js
+++ src/services/db.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const setting = {
 	connectionLimit: 10,
-	host: "127.0.0.1",
-	user: "root",
-	password: "Zafyree@3003",
-	database: "bed_ca1",
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 	multipleStatements: true,
 	dateStrings: true,
 };
